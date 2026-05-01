8f7741a06a9564a5cd24addc5d28143e29accebe
alonsoC21 <153787788+alonsoC21@users.noreply.github.com>
2026-04-30 21:53:15 -0600

    Add files via upload

---
+++ Makefile/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hola desde un ejemplo basico de Makefile.\n");
+    printf("Este programa fue compilado y ejecutado con make.\n");
+    return 0;
+}
